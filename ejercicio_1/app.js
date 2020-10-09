5e5372dae41db48fc27b5fac03a4f036748efd77
marcoskukuchka <marcos.kukuchka@gmail.com>
2020-10-09 19:55:52 -0300

    ej1

--- ejercicio_1/app.js
+++ ejercicio_1/app.js
@@ -1,11 +1,14 @@
-
 const obtenerChiste = require("./library");
 
 // Codigo funcion callback
 
+let contarChiste = function(chiste) {
+        let guardaChiste = chiste[0];
+        console.log(guardaChiste.setup);
+        console.log(guardaChiste.punchline);
 
-
-// Fin codigo
+    }
+    // Fin codigo
 
 // Usar la funcion obtenerChiste(funcionCallback) en donde funcionCallback es una función que recibe el objeto chiste extraido
-obtenerChiste();
+obtenerChiste(contarChiste);
